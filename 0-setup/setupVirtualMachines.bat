ad80bcebd3573429757a60b0146ad424eb288b74
Judith Kahrer <judith.kahrer@omegapoint.se>
2018-12-07 12:10:42 +0100

    Change version of boot2docker

--- 0-setup/setupVirtualMachines.bat
+++ 0-setup/setupVirtualMachines.bat
@@ -6,19 +6,19 @@ echo ****************************************************
 
 echo.
 echo Creating a new virtual machine on Oracle Virtualbox called 'docker-security-1'.
-echo ^> docker-machine create --driver virtualbox --virtualbox-cpu-count ^"2^" --virtualbox-memory ^"1024^" docker-security-1
-docker-machine create --driver virtualbox --virtualbox-cpu-count "2" --virtualbox-memory "1024" docker-security-1
+
+echo ^> docker-machine create --driver virtualbox --virtualbox-cpu-count ^"2^" --virtualbox-memory ^"1024^"  --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso docker-security-1
+docker-machine create --driver virtualbox --virtualbox-cpu-count "2" --virtualbox-memory "1024" 	--virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso docker-security-1
 
 echo.
 echo Configuring 'docker-security-1' as the Docker Engine for the current terminal.
-REM echo ^> @FOR /f ^"tokens=*^" %%i IN ('docker-machine env docker-security-1') DO @%%i
 @FOR /f "tokens=*" %%i IN ('docker-machine env docker-security-1') DO @%%i
 echo  Created machine 1/2
 
 echo.
 echo Creating a new virtual machine on Oracle Virtualbox called 'docker-security-2'.
-echo ^> docker-machine create --driver virtualbox docker-security-2
-docker-machine create --driver virtualbox docker-security-2
+echo ^> docker-machine create --driver virtualbox --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso docker-security-2
+docker-machine create --driver virtualbox 	--virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso docker-security-2
 echo Created machine 2/2
 
 echo.
--- 0-setup/setupVirtualMachines.sh
+++ 0-setup/setupVirtualMachines.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 printf "****************************************************\n"
 printf "This script creates two new virtual machines on Oracle Virtualbox called '%s' and '%s'.\n" "docker-security-1" "docker-security-2"
-printf "'%s' is setup to be the default Docker Engine for the current terminal.\n"
+printf "'%s' is setup to be the default Docker Engine for the current terminal.\n" "docker-security-1"
 printf "****************************************************\n"
 
 printf "Creating a new virtual machine on Oracle Virtualbox called '%s'\n" "docker-security-1"
@@ -9,7 +9,9 @@ docker-machine create \
     --driver virtualbox \
     --virtualbox-cpu-count "2" \
     --virtualbox-memory "1024" \
-    docker-security-1
+	--virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso \
+	docker-security-1
+	
 
 printf "\n"
 printf "Configuring '%s' as the Docker Engine for the current terminal." "docker-security-1"
@@ -17,9 +19,13 @@ eval $(docker-machine env docker-security-1)
 printf "\n"
 printf "Created machine 1/2"
 
-printf "\n"
+printf "\n\n"
 printf "Creating a new virtual machine on Oracle Virtualbox called '%s'\n" "docker-security-2"
-docker-machine create --driver virtualbox docker-security-2
+docker-machine create \
+    --driver virtualbox \
+	--virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/v18.05.0-ce/boot2docker.iso \
+	docker-security-2
+	
 printf "\n"
 printf "Created machine 2/2"
 
